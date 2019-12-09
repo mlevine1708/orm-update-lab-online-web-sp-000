2b133c18f9ffd3b46505d48a996aef40a88c26a1
Micah Levine <micah.levine@outlook.com>
2019-12-09 03:07:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,10 +46,16 @@ attr_reader :id
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
+  #def self.create(name:, grade:)
+  #  student = Student.new(name, grade)
+  #  student.save
+  #  student
+  #end
   def self.create(name:, grade:)
     student = Student.new(name, grade)
     student.save
     student
   end
+
 
 end
