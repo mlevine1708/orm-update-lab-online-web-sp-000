8829118edf74ef01cd65b4755d3e92cefbe9640b
Micah Levine <micah.levine@outlook.com>
2019-12-09 02:30:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,5 +11,14 @@ attr_reader :id
     @id = id
   end
 
-
+  def create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end  
 end
