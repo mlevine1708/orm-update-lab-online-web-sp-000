aa75d755cfe6e5526adce01ad290043c242d8e64
Micah Levine <micah.levine@outlook.com>
2019-12-09 02:55:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,12 +29,16 @@ attr_reader :id
   end
 
   def save
-    sql = <<-SQL
-      INSERT INTO students (name, grade)
-      VALUES (?, ?)
-    SQL
-
+    if self.id 
+      self.update 
+    else 
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+      SQL
     DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_row() FROM students") [0][0]
+    end 
   end
 
 end
