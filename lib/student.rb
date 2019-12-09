9eb1d71da625dfb2a663ac4d3a74a82f8be5ffa2
Micah Levine <micah.levine@outlook.com>
2019-12-09 03:11:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,11 +53,11 @@ attr_reader :id
   end
 
   def self.new_from_db(row)
-    student = self.new
+    student = self.new(id, name, grade)
     student.id = row[0]
     student.name = row[1]
     student.grade = row[2]
     student
-  end 
+  end
 
 end
